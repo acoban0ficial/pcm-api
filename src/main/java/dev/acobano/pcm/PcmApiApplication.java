9e4a4011f63ecf946aa6ec6387134f8d7c200cb6
acobano <132894262+acobdev@users.noreply.github.com>
2026-03-01 23:19:39 +0100

    feat: Sincro con repo remoto + build proyecto levantado

---
+++ src/main/java/dev/acobano/pcm/PcmApiApplication.java
@@ -0,0 +1,13 @@
+package dev.acobano.pcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcmApiApplication.class, args);
+	}
+
+}
