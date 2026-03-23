37acef4957b8ee163ab583a37d0b8b54f931ab78
acobano <alvcobsou@gmail.com>
2026-03-23 19:37:19 +0100

    fix: Implementada anotacion endpoint PUT para Projects

--- src/main/java/dev/acobano/pcm/controller/rest/ProjectRestController.java
+++ src/main/java/dev/acobano/pcm/controller/rest/ProjectRestController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -168,6 +169,11 @@ public class ProjectRestController {
                 ).body(response);
     }
 
+    @PutMapping(
+            value = "/{projectId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<ProjectResponseDTO> updateProject(
             @PathVariable("projectId") UUID projectId,
             @Valid @RequestBody ProjectPutRequestDTO request
